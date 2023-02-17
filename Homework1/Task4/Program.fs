7a1737eba8a2df273706624b2c809db9c49be210
Nikolai Ponomarev <wowasterdev@gmail.com>
2023-02-17 21:52:19 +0300

    Add solution

---
+++ Homework1/Task4/Program.fs
@@ -0,0 +1,4 @@
+﻿let arrayOfPowersOfTwo n m =
+    [ for power in n .. (n + m) -> 1UL <<< power ]
+
+arrayOfPowersOfTwo 48 15 |> printfn "%A"
---
+++ Homework1/Task5/Program.fs
@@ -0,0 +1,14 @@
+﻿let findFirstInList value list =
+    let rec findFirstInList list index =
+        match list with
+        | [] -> None
+        | head :: tail ->
+            if head = value then
+                Some(index)
+            else
+                findFirstInList tail (index + 1)
+
+    findFirstInList list 0
+
+let l = [ "asjdfnajfd"; "rules"; "bubbles"; "tea" ]
+printfn $"""{findFirstInList "tea" l} {findFirstInList "a" l}"""
