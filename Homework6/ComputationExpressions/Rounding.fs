da654b3933322808d5b52cec0d21731b70b540ef
Nikolai Ponomarev <wowasterdev@gmail.com>
2023-05-20 12:21:19 +0300

    Fix rounding order

--- Homework6/ComputationExpressions/Rounding.fs
+++ Homework6/ComputationExpressions/Rounding.fs
@@ -5,7 +5,7 @@ open System
 type RoundingBuilder(nOfSignificantDigits: int) =
     member this.nOfSignificantDigits = nOfSignificantDigits
 
-    member this.Bind(x, f: _ -> float) = Math.Round(f x, nOfSignificantDigits)
+    member this.Bind(x: float, f: _ -> float) = f (Math.Round(x, nOfSignificantDigits))
     member this.Return(x) = x
     member this.ReturnFrom(x: float) = Math.Round(x, nOfSignificantDigits)
 
