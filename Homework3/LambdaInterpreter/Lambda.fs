392762e49deb7a1f57c330fe0bd1f63b2bfcc15f
Nikolai Ponomarev <wowasterdev@gmail.com>
2023-05-20 12:39:54 +0300

    Code formatting and renaming

--- Homework3/LambdaInterpreter/Lambda.fs
+++ Homework3/LambdaInterpreter/Lambda.fs
@@ -8,9 +8,9 @@ type Term =
 module Lambda =
     let rec getFreeVars term =
         match term with
-        | Var name -> Set.singleton(name)
-        | Abs (name, innerTerm) -> (getFreeVars innerTerm).Remove(name)
-        | App (leftTerm, rightTerm) -> Set.union (getFreeVars leftTerm) (getFreeVars rightTerm)
+        | Var name -> Set.singleton name
+        | Abs(name, innerTerm) -> (getFreeVars innerTerm).Remove(name)
+        | App(leftTerm, rightTerm) -> Set.union (getFreeVars leftTerm) (getFreeVars rightTerm)
 
     let rec getFreshVar varName vars =
         match Set.contains varName vars with
@@ -21,23 +21,23 @@ module Lambda =
         let rec helper term =
             match term with
             | Var name when name = varName -> subTerm
-            | Var name -> Var name
-            | Abs (name, innerTerm) when name = varName -> Abs(name, innerTerm)
-            | Abs (name, innerTerm) -> Abs(name, helper innerTerm)
-            | App (leftTerm, rightTerm) -> App(helper leftTerm, helper rightTerm)
+            | Var _ -> term
+            | Abs(name, _) when name = varName -> term
+            | Abs(name, innerTerm) -> Abs(name, helper innerTerm)
+            | App(leftTerm, rightTerm) -> App(helper leftTerm, helper rightTerm)
 
         helper term
 
     let renameVars term vars =
         let rec helper term =
             match term with
-            | Abs (name, innerTerm) when Set.contains name vars ->
+            | Abs(name, innerTerm) when Set.contains name vars ->
                 let newVarName = getFreshVar name vars
                 let subTerm = substituteTerm innerTerm name (Var newVarName)
                 Abs(newVarName, helper subTerm)
-            | Abs (name, innerTerm) -> Abs(name, helper innerTerm)
-            | App (leftTerm, rightTerm) -> App(helper leftTerm, helper rightTerm)
-            | term -> term
+            | Abs(name, innerTerm) -> Abs(name, helper innerTerm)
+            | App(leftTerm, rightTerm) -> App(helper leftTerm, helper rightTerm)
+            | _ -> term
 
         helper term
 
@@ -47,15 +47,15 @@ module Lambda =
             | true -> term
             | false ->
                 match term with
-                | Var name -> Var name
-                | Abs (name, innerTerm) -> Abs(name, helper innerTerm (seenTerms.Add(term)))
-                | App (Abs (name, innerTerm), rightTerm) ->
+                | Var _ -> term
+                | Abs(name, innerTerm) -> Abs(name, helper innerTerm (seenTerms.Add(term)))
+                | App(Abs(name, innerTerm), rightTerm) ->
                     let newTerm =
                         substituteTerm (renameVars innerTerm (getFreeVars rightTerm)) name rightTerm
 
                     helper newTerm (seenTerms.Add(term))
-                | App (App _ as leftTerm, rightTerm) ->
+                | App(App _ as leftTerm, rightTerm) ->
                     helper (App(helper leftTerm (seenTerms.Add(term)), rightTerm)) (seenTerms.Add(term))
-                | App (leftTerm, rightTerm) -> App(leftTerm, helper rightTerm (seenTerms.Add(term)))
+                | App(leftTerm, rightTerm) -> App(leftTerm, helper rightTerm (seenTerms.Add(term)))
 
         helper term Set.empty
--- Homework3/LambdaTest/LambdaTest.fs
+++ Homework3/LambdaTest/LambdaTest.fs
@@ -4,32 +4,38 @@ open NUnit.Framework
 open FsUnit
 open LambdaInterpreter
 
-let K name1 name2 = Abs(name1, Abs(name2, Var(name1)))
-let K_star name1 name2 = Abs(name1, Abs(name2, Var(name2)))
+let k name1 name2 = Abs(name1, Abs(name2, Var(name1)))
+let kStar name1 name2 = Abs(name1, Abs(name2, Var(name2)))
 
-let S name1 name2 name3=
+let s name1 name2 name3 =
     Abs(name1, Abs(name2, Abs(name3, App(App(Var(name1), Var(name3)), App(Var(name2), Var(name3))))))
 
-let I name = Abs(name, Var(name))
+let i name = Abs(name, Var(name))
 
 let omega name = Abs(name, App(Var(name), Var(name)))
 
-let Omega name = App(omega name, omega name )
+let bigOmega name = App(omega name, omega name)
 
 
 [<Test>]
-let ``S K K = I`` () = Lambda.eval (App(App(S "x" "y" "z", K "x" "y" ), K "x" "y" )) |> should equal (I "z")
+let ``S K K = I`` () =
+    Lambda.eval (App(App(s "x" "y" "z", k "x" "y"), k "x" "y"))
+    |> should equal (i "z")
 
 [<Test>]
-let ``Ω doesn't have normal form`` () = Lambda.eval (Omega "x") |> should equal (Omega "x")
+let ``Ω doesn't have normal form`` () =
+    Lambda.eval (bigOmega "x") |> should equal (bigOmega "x")
 
 let toRename = App(Abs("x", Abs("y", App(Var("x"), Var("y")))), Var "y")
 
 [<Test>]
-let ``Variable rename does occur`` () = Lambda.eval toRename |> should equal (Abs ("'y", App (Var "y", Var "'y")))
+let ``Variable rename does occur`` () =
+    Lambda.eval toRename |> should equal (Abs("'y", App(Var "y", Var "'y")))
 
 [<Test>]
-let ``K I = K_*`` () = Lambda.eval (App(K "x" "y", I "x")) |> should equal (K_star "y" "x")
+let ``K I = K_*`` () =
+    Lambda.eval (App(k "x" "y", i "x")) |> should equal (kStar "y" "x")
 
 [<Test>]
-let ``ω I = I``() = Lambda.eval (App(omega "s", I "x")) |> should equal (I "x")
+let ``ω I = I`` () =
+    Lambda.eval (App(omega "s", i "x")) |> should equal (i "x")
